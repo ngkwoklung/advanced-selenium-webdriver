c5e0ab41186d24b1a1e4839ddb7ea72e2255bcfa
Jimmy Ng <ngkwoklung@gmail.com>
2022-10-25 17:11:03 +0100

    Horizontal slider test

---
+++ src/test/java/com/herokuapp/theinternet/horizontalslidertests/HorizontalSliderTests.java
@@ -0,0 +1,19 @@
+package com.herokuapp.theinternet.horizontalslidertests;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.HorizontalSliderPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HorizontalSliderTests extends TestUtilities {
+    @Test
+    public void sliderTest() {
+        log.info("Starting sliderTest");
+        HorizontalSliderPage horizontalSliderPage = new HorizontalSliderPage(driver, log);
+        horizontalSliderPage.openPage();
+        double value = 3.5;
+        horizontalSliderPage.setSliderTo(value);
+        String sliderValue = horizontalSliderPage.getSliderValue();
+        Assert.assertTrue(sliderValue.equals(String.valueOf(value)), "Range is not correct. It is: " + sliderValue);
+    }
+}
