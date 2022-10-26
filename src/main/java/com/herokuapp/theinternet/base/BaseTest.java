e3f4b49255311598ac328f81a9d2fbeb6cf170e0
Jimmy Ng <ngkwoklung@gmail.com>
2022-10-26 14:06:54 +0100

    Screenshots

--- src/main/java/com/herokuapp/theinternet/base/BaseTest.java
+++ src/main/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -9,14 +9,19 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.lang.reflect.Method;
+
 public class BaseTest {
 
 	protected WebDriver driver;
 	protected Logger log;
+	protected String testSuiteName;
+	protected String testName;
+	protected String testMethodName;
 
 	@Parameters({ "browser" })
 	@BeforeMethod(alwaysRun = true)
-	public void setUp(@Optional("chrome") String browser, ITestContext ctx) {
+	public void setUp(Method method, @Optional("chrome") String browser, ITestContext ctx) {
 		String testName = ctx.getCurrentXmlTest().getName();
 		log = LogManager.getLogger(testName);
 		
@@ -31,6 +36,10 @@ public class BaseTest {
 		}
 		
 		driver.manage().window().maximize();
+
+		this.testSuiteName = ctx.getSuite().getName();
+		this.testName = testName;
+		this.testMethodName = method.getName();
 	}
 
 	@AfterMethod(alwaysRun = true)
--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -1,6 +1,14 @@
 package com.herokuapp.theinternet.base;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestUtilities extends BaseTest {
 
@@ -19,5 +27,30 @@ public class TestUtilities extends BaseTest {
 				{2,"logo.png"},
 				{3,"text.txt"}
 		};
+	}
+
+	protected void takeScreenshot(String fileName) {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir")
+				+ File.separator + "test-output"
+				+ File.separator + "screenshots"
+				+ File.separator + getTodaysDate()
+				+ File.separator + testSuiteName
+				+ File.separator + testName
+				+ File.separator + testMethodName
+				+ File.separator + getSystemTime()
+				+ " " + fileName + ".png";
+		try {
+			FileUtils.copyFile(scrFile, new File(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private static String getTodaysDate() {
+		return (new SimpleDateFormat("yyyyMMdd").format(new Date()));
+	}
+
+	private String getSystemTime() {
+		return (new SimpleDateFormat("HHmmssSSS").format(new Date()));
 	}
 }
